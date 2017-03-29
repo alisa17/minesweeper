82d4acc4fa31aa1f6cede43293f8b00e0f4e1ad0
Alisa Yunusova <alisa.yunusova@gmail.com>
2017-03-29 20:01:05 +1300

    Replaced board object with function output

--- minesweeper.js
+++ minesweeper.js
@@ -1,72 +1,25 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = {
-    cells: [{
-        row: 0,
-        col: 0,
-        isMine: false,
-        isMarked: false,
-        hidden: true,
-        surroundingMines: 0
-    }, {
-        row: 0,
-        col: 1,
-        isMine: false,
-        isMarked: false,
-        hidden: true,
-        surroundingMines: 0
-    }, {
-        row: 0,
-        col: 2,
-        isMine: true,
-        isMarked: false,
-        hidden: true,
-        surroundingMines: 0
-    }, {
-        row: 1,
-        col: 0,
-        isMine: true,
-        isMarked: false,
-        hidden: true,
-        surroundingMines: 0
-    }, {
-        row: 1,
-        col: 1,
-        isMine: false,
-        isMarked: false,
-        hidden: true,
-        surroundingMines: 0
-    }, {
-        row: 1,
-        col: 2,
-        isMine: false,
-        isMarked: false,
-        hidden: true,
-        surroundingMines: 0
-    }, {
-        row: 2,
-        col: 0,
-        isMine: false,
-        isMarked: false,
-        hidden: true,
-        surroundingMines: 0
-    }, {
-        row: 2,
-        col: 1,
-        isMine: false,
-        isMarked: false,
-        hidden: true,
-        surroundingMines: 0
-    }, {
-        row: 2,
-        col: 3,
-        isMine: false,
-        isMarked: false,
-        hidden: true,
-        surroundingMines: 0
-    }]
-};
+var board = createBoard(4, 4);
+
+function createBoard(x, y) {
+    var board = {};
+    board.cells = [];
+    for (var i = 0; i < x; i++) {
+        for (var j = 0; j < y; j++) {
+            board.cells.push({
+                row: i,
+                col: j,
+                isMine: true,
+                isMarked: false,
+                hidden: true
+            })
+        }
+    }
+    return board;
+}
+
 
 function startGame(cell) {
 
@@ -92,10 +45,10 @@ function checkForWin() {
     // detected that they've won, that is!)
     //   lib.displayMessage('You win!')
 
-    for (var i=0; i< board.cells.length; i++) {
-      if (board.cells[i].isMarked === true && board.cells[i].isMine == true) {
-        lib.displayMessage('You win!');
-      }
+    for (var i = 0; i < board.cells.length; i++) {
+        if (board.cells[i].isMarked === true && board.cells[i].isMine == true) {
+            lib.displayMessage('You win!');
+        }
     }
 }
 
